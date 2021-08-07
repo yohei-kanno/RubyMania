c7452c6b128f3ce7d25a891b897cc7015ab1e9fe
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-08-07 13:03:23 +0900

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,19 @@
-# 99.times do |i|
-#   LevelStatus.create(
-#     level: i + 2,
-#     required_experience_points: 50 * (i + 2)
-#   )
-# end
+99.times do |i|
+  LevelStatus.create(
+    level: i + 2,
+    required_experience_points: 50 * (i + 2)
+  )
+end
 
-# Category.create(
-#   name: "String(文字列)"
-# )
-# Category.create(
-#   name: "Integer(数値)"
-# )
-# Category.create(
-#   name: "Array(配列)"
-# )
-# Category.create(
-#   name: "Hash(ハッシュ)"
-# )
-
+Category.create(
+  name: "String(文字列)"
+)
+Category.create(
+  name: "Integer(数値)"
+)
+Category.create(
+  name: "Array(配列)"
+)
+Category.create(
+  name: "Hash(ハッシュ)"
+)
