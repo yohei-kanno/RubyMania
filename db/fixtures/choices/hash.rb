f8c2bde8fc3d721e8b8250f6538a77b068e0d4cc
yohei-kanno <tkro.812.wkrni@icloud.com>
2021-09-06 12:00:34 +0900

    Fix: cssを変更

--- db/fixtures/choices/hash.rb
+++ db/fixtures/choices/hash.rb
@@ -4,25 +4,25 @@ is_true = '「true」と表示される'
 is_false = '「false」と表示される'
 
 # h_question1
-$h_choice1 = '"apple"と表示される'
+$h_choice1 = '"foo"と表示される'
 $h_choice2 = '「:a」と表示される'
 $h_choice3 = is_nil
 $h_choice4 = is_exception
 
 # h_question2
-$h_choice5 = '"apple"と表示される'
+$h_choice5 = '"foo"と表示される'
 $h_choice6 = '「:a」と表示される'
 $h_choice7 = is_nil
 $h_choice8 = is_exception
 
 # h_question3
-$h_choice9 = '["apple", "orange", "strawberry", "melon"]と表示される'
+$h_choice9 = '["foo", "bar", "baz", "qux"]と表示される'
 $h_choice10 = '[:a, :b, :c, :d]と表示される'
 $h_choice11 = '[nil]と表示される'
 $h_choice12 = is_exception
 
 # h_question4
-$h_choice13 = '["apple", "orange", "strawberry", "melon"]と表示される'
+$h_choice13 = '["foo", "bar", "baz", "qux"]と表示される'
 $h_choice14 = '[:a, :b, :c, :d]と表示される'
 $h_choice15 = '[nil]と表示される'
 $h_choice16 = is_exception
@@ -30,121 +30,121 @@ $h_choice16 = is_exception
 # h_question5
 $h_choice17 = is_exception
 $h_choice18 = '[nil]と表示される'
-$h_choice19 = '["apple", "orange"]と表示される'
+$h_choice19 = '["foo", "bar"]と表示される'
 $h_choice20 = '[:a, :b]と表示される'
 
 # h_question6
-$h_choice21 = '{:a=>"apple"}と表示される'
-$h_choice22 = '{:a=>"apple", :b=>"orange"}と表示される'
-$h_choice23 = '{:a=>"apple", :b=>"orange", :c=>"strawberry"}と表示される'
+$h_choice21 = '{:a=>"foo"}と表示される'
+$h_choice22 = '{:a=>"foo", :b=>"bar"}と表示される'
+$h_choice23 = '{:a=>"foo", :b=>"bar", :c=>"baz"}と表示される'
 $h_choice24 = is_exception
 
 # h_question7
-$h_choice25 = '{:a=>"apple"}と表示される'
-$h_choice26 = '{:a=>"apple", :b=>"orange"}と表示される'
-$h_choice27 = '{:a=>"apple", :b=>"orange", :c=>"strawberry"}と表示される'
+$h_choice25 = '{:a=>"foo"}と表示される'
+$h_choice26 = '{:a=>"foo", :b=>"bar"}と表示される'
+$h_choice27 = '{:a=>"foo", :b=>"bar", :c=>"baz"}と表示される'
 $h_choice28 = '{}と表示される'
 
 # h_question8
-$h_choice29 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice30 = '"strawberry"と表示される'
+$h_choice29 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice30 = '"baz"と表示される'
 $h_choice31 = '"grape"と表示される'
 $h_choice32 = is_exception
 
 # h_question9
-$h_choice33 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice34 = '"strawberry"と表示される'
+$h_choice33 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice34 = '"baz"と表示される'
 $h_choice35 = '"grape"と表示される'
 $h_choice36 = is_exception
 
 # h_question10
-$h_choice37 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice38 = '{:b=>"orange", :c=>"strawberry", :d=>"melon"}と表示される'
-$h_choice39 = '{a: "apple"}'
+$h_choice37 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice38 = '{:b=>"bar", :c=>"baz", :d=>"qux"}と表示される'
+$h_choice39 = '{a: "foo"}'
 $h_choice40 = is_exception
 
 # h_question11
-$h_choice41 = '{b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice42 = '{a: "apple"}と表示される'
-$h_choice43 = '"apple"と表示される'
+$h_choice41 = '{b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice42 = '{a: "foo"}と表示される'
+$h_choice43 = '"foo"と表示される'
 $h_choice44 = is_exception
 
 # h_question12
-$h_choice45 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice46 = '{b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice47 = '{a: "apple"}と表示される'
-$h_choice48 = '"apple"と表示される'
+$h_choice45 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice46 = '{b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice47 = '{a: "foo"}と表示される'
+$h_choice48 = '"foo"と表示される'
 
 # h_question13
-$h_choice49 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice50 = '{b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice51 = '{a: "apple"}と表示される'
-$h_choice52 = '"apple"と表示される'
+$h_choice49 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice50 = '{b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice51 = '{a: "foo"}と表示される'
+$h_choice52 = '"foo"と表示される'
 
 # h_question14
-$h_choice53 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice54 = '{b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice55 = '{a: "apple"}と表示される'
+$h_choice53 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice54 = '{b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice55 = '{a: "foo"}と表示される'
 $h_choice56 = is_exception
 
 # h_question15
-$h_choice57 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice58 = '{b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice59 = '{a: "apple"}と表示される'
+$h_choice57 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice58 = '{b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice59 = '{a: "foo"}と表示される'
 $h_choice60 = is_exception
 
 # h_question16
-$h_choice61 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice62 = '{b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice63 = '{a: "apple"}と表示される'
+$h_choice61 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice62 = '{b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice63 = '{a: "foo"}と表示される'
 $h_choice64 = is_exception
 
 # h_question17
-$h_choice65 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice66 = '{b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice67 = '{b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice68 = '{a: "apple"}と表示される'
+$h_choice65 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice66 = '{b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice67 = '{b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice68 = '{a: "foo"}と表示される'
 
 # h_question18
-$h_choice69 = '{a: "apple", b: "orange", c: "strawberry", d: "melon", a: "orange"}と表示される'
-$h_choice70 = '{a: apple, b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice71 = '{a: "orange"}と表示される'
+$h_choice69 = '{a: "foo", b: "bar", c: "baz", d: "qux", a: "bar"}と表示される'
+$h_choice70 = '{a: foo, b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice71 = '{a: "bar"}と表示される'
 $h_choice72 = is_exception
 
 # h_question19
-$h_choice73 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice74 = '[b:, "orange", c:, "strawberry", d:, "melon"]と表示される'
-$h_choice75 = '[:a, "apple"]と表示される'
+$h_choice73 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice74 = '[b:, "bar", c:, "baz", d:, "qux"]と表示される'
+$h_choice75 = '[:a, "foo"]と表示される'
 $h_choice76 = is_exception
 
 # h_question20
-$h_choice77 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice78 = '{b:, "orange", c:, "strawberry", d:, "melon"}と表示される'
-$h_choice79 = '[:a, "apple"]と表示される'
+$h_choice77 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice78 = '{b:, "bar", c:, "baz", d:, "qux"}と表示される'
+$h_choice79 = '[:a, "foo"]と表示される'
 $h_choice80 = is_exception
 
 # h_question21
-$h_choice81 = '{:a=>"apple", :b=>"orange", :c=>"strawberry", :d=>"melon"}と表示される'
-$h_choice82 = '{a: "apple", b: "orange" }と表示される'
-$h_choice83 = '{:a, "apple"}と表示される'
+$h_choice81 = '{:a=>"foo", :b=>"bar", :c=>"baz", :d=>"qux"}と表示される'
+$h_choice82 = '{a: "foo", b: "bar" }と表示される'
+$h_choice83 = '{:a, "foo"}と表示される'
 $h_choice84 = is_exception
 
 # h_question22
-$h_choice85 = '{:a=>"apple", :b=>"orange", :c=>"strawberry", :d=>"melon"}と表示される'
-$h_choice86 = '{a: "apple", b: "orange" }と表示される'
-$h_choice87 = '{c: "strawberry", d: "melon"}と表示される'
+$h_choice85 = '{:a=>"foo", :b=>"bar", :c=>"baz", :d=>"qux"}と表示される'
+$h_choice86 = '{a: "foo", b: "bar" }と表示される'
+$h_choice87 = '{c: "baz", d: "qux"}と表示される'
 $h_choice88 = is_exception
 
 # h_question23
-$h_choice89 = '{:a=>"apple", :b=>"orange", :c=>"strawberry", :d=>"melon"}と表示される'
-$h_choice90 = '{a: "apple", b: "orange" }と表示される'
-$h_choice91 = '{c: "strawberry", d: "melon"}と表示される'
+$h_choice89 = '{:a=>"foo", :b=>"bar", :c=>"baz", :d=>"qux"}と表示される'
+$h_choice90 = '{a: "foo", b: "bar" }と表示される'
+$h_choice91 = '{c: "baz", d: "qux"}と表示される'
 $h_choice92 = is_exception
 
 # h_question24
-$h_choice93 = '{:a=>"apple", :b=>"orange", :c=>"strawberry", :d=>"melon"}と表示される'
-$h_choice94 = '{a: "apple"}と表示される'
-$h_choice95 = '{"apple"=>:a, "orange"=>:b, "strawberry"=>:c, "melon"=>:d}と表示される'
+$h_choice93 = '{:a=>"foo", :b=>"bar", :c=>"baz", :d=>"qux"}と表示される'
+$h_choice94 = '{a: "foo"}と表示される'
+$h_choice95 = '{"foo"=>:a, "bar"=>:b, "baz"=>:c, "qux"=>:d}と表示される'
 $h_choice96 = is_exception
 
 # h_question25
@@ -184,38 +184,38 @@ $h_choice119 = '「4」と表示される'
 $h_choice120 = '「0」と表示される'
 
 # h_question31
-$h_choice121 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
+$h_choice121 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
 $h_choice122 = '「:a
 :b
 :c
 :d
 」と表示される'
 
-$h_choice123 = '「"apple"
-"orange"
-"strawberry"
-"melon"」と表示される'
+$h_choice123 = '「"foo"
+"bar"
+"baz"
+"qux"」と表示される'
 
 $h_choice124 = is_exception
 
 # h_question32
-$h_choice125 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
+$h_choice125 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
 $h_choice126 = '「:a
 :b
 :c
 :d
 」と表示される'
 
-$h_choice127 = '「"apple"
-"orange"
-"strawberry"
-"melon"」と表示される'
+$h_choice127 = '「"foo"
+"bar"
+"baz"
+"qux"」と表示される'
 
 $h_choice128 = is_exception
 
 # h_question33
-$h_choice129 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice130 = '[[:a, "apple"], [:b, "orange"], [:c, "strawberry"], [:d, "melon"]]と表示される'
+$h_choice129 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice130 = '[[:a, "foo"], [:b, "bar"], [:c, "baz"], [:d, "qux"]]と表示される'
 
 $h_choice131 = '[nil]と表示される'
 $h_choice132 = is_exception
@@ -233,14 +233,14 @@ $h_choice139 = is_nil
 $h_choice140 = is_exception
 
 # h_question36
-$h_choice141 = '{:a=>"apple", :c=>"strawberry"}と表示される'
-$h_choice142 = '{:a=>"apple", :b=>nil, :c=>"strawberry", :d=>nil}と表示される'
+$h_choice141 = '{:a=>"foo", :c=>"baz"}と表示される'
+$h_choice142 = '{:a=>"foo", :b=>nil, :c=>"baz", :d=>nil}と表示される'
 $h_choice143 = '{nil, nil}と表示される'
 $h_choice144 = is_exception
 
 # h_question37
-$h_choice145 = '{:a=>"apple", :c=>"strawberry"}と表示される'
-$h_choice146 = '{:a=>"apple", :b=>nil, :c=>"strawberry", :d=>nil}と表示される'
+$h_choice145 = '{:a=>"foo", :c=>"baz"}と表示される'
+$h_choice146 = '{:a=>"foo", :b=>nil, :c=>"baz", :d=>nil}と表示される'
 $h_choice147 = '{nil, nil}と表示される'
 $h_choice148 = is_exception
 
@@ -257,7 +257,7 @@ $h_choice155 = '「3」と表示される'
 $h_choice156 = '「4」と表示される'
 
 # h_question40
-$h_choice157 = '{a: "apple", b: "orange", c: "strawberry", d: "melon"}と表示される'
-$h_choice158 = '{a: "apple", b: "orange", c: "strawberry"}と表示される'
-$h_choice159 = '{d: "melon"}と表示される'
+$h_choice157 = '{a: "foo", b: "bar", c: "baz", d: "qux"}と表示される'
+$h_choice158 = '{a: "foo", b: "bar", c: "baz"}と表示される'
+$h_choice159 = '{d: "qux"}と表示される'
 $h_choice160 = is_exception
--- db/fixtures/questions/hash.rb
+++ db/fixtures/questions/hash.rb
@@ -1,189 +1,189 @@
 $h_question1 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj[0]'
 
 $h_question2 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj[:a]'
 
 $h_question3 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj.keys'
 
 $h_question4 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj.values'
 
 $h_question5 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj.values_at(:a,:b)'
 
 $h_question6 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj.select{|key, value| value.include?("a")}'
 
 $h_question7 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj.select{|key, value| value.include?(":a")}'
 
 $h_question8 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj[:c].update!("grape")'
 
 $h_question9 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 obj[:c] = "grape"
 p obj[:c]'
 
 $h_question10 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 obj.delete(:a)
 p obj'
 
 $h_question11 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj.delete(:a)'
 
 $h_question12 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
-p obj.reject{|key, value| value == "apple" }'
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
+p obj.reject{|key, value| value == "foo" }'
 
 $h_question13 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
-obj.reject{|key, value| value == "apple" }
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
+obj.reject{|key, value| value == "foo" }
 p obj'
 
 $h_question14 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
-obj.reject!{|key, value| value == "apple" }
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
+obj.reject!{|key, value| value == "foo" }
 p obj'
 
 $h_question15 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
-obj.delete_if{|key, value| value == "apple" }
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
+obj.delete_if{|key, value| value == "foo" }
 p obj'
 
 $h_question16 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
-obj.delete_if{|key, value| value == "apple" }
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
+obj.delete_if{|key, value| value == "foo" }
 p obj'
 
 $h_question17 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
-obj.delete{|key, value| value == "apple" }
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
+obj.delete{|key, value| value == "foo" }
 p obj'
 
 $h_question18 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
-obj.replace(a: "orange")
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
+obj.replace(a: "bar")
 p obj'
 
 $h_question19 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj.shift'
 
 $h_question20 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 
 obj.shift
 p obj'
 
 $h_question21 =
-  'obj1 = {a: "apple", b: "orange" }
-obj2 = {c: "strawberry", d: "melon"}
+  'obj1 = {a: "foo", b: "bar" }
+obj2 = {c: "baz", d: "qux"}
 
 obj1.merge(obj2)
 p obj1'
 
 $h_question22 =
-  'obj1 = {a: "apple", b: "orange" }
-obj2 = {c: "strawberry", d: "melon"}
+  'obj1 = {a: "foo", b: "bar" }
+obj2 = {c: "baz", d: "qux"}
 
 obj1.merge!(obj2)
 p obj1'
 
 $h_question23 =
-  'obj1 = {a: "apple", b: "orange" }
-obj2 = {c: "strawberry", d: "melon"}
+  'obj1 = {a: "foo", b: "bar" }
+obj2 = {c: "baz", d: "qux"}
 
 obj1.merge!(obj2)
 p obj2'
 
 $h_question24 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 
 p obj.invert'
 
 $h_question25 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 
-p obj.key?("apple")'
+p obj.key?("foo")'
 
 $h_question26 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 
 p obj.member?(:b)'
 
 $h_question27 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 
 p obj.clear.size'
 
 $h_question28 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj.empty?'
 
 $h_question29 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj.has_value?(:a)'
 
 $h_question30 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
-p obj.has_value?("melon")'
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
+p obj.has_value?("qux")'
 
 $h_question31 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 obj.each_key{|k| p k}'
 
 $h_question32 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 obj.each_value{|v| p v}'
 
 $h_question33 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj.sort'
 
 $h_question34 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj.to_a.eql?(obj.sort)'
 
 $h_question35 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 p obj.to_a.equal?(obj.sort)'
 
 $h_question36 =
-  'obj = {a: "apple", b: nil, c: "strawberry", d: nil}
+  'obj = {a: "foo", b: nil, c: "baz", d: nil}
 
 obj.compact
 p obj'
 
 $h_question37 =
-  'obj = {a: "apple", b: nil, c: "strawberry", d: nil}
+  'obj = {a: "foo", b: nil, c: "baz", d: nil}
 
 obj.compact!
 p obj'
 
 $h_question38 =
-  'obj = {a: "apple", b: nil, c: "strawberry", d: nil}
+  'obj = {a: "foo", b: nil, c: "baz", d: nil}
 
 p obj.flatten == obj.to_a.flatten'
 
 $h_question39 =
-  'obj = {a: "apple", b: nil, c: "strawberry", d: nil}
+  'obj = {a: "foo", b: nil, c: "baz", d: nil}
 
 p obj.keys.count'
 
 $h_question40 =
-  'obj = {a: "apple", b: "orange", c: "strawberry", d: "melon"}
+  'obj = {a: "foo", b: "bar", c: "baz", d: "qux"}
 
 p obj.pop'
