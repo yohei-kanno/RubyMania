64e1e8d28370c0c5e28a43a25e2435874cb3a9d1
yohei-kanno <tkro.812.wkrni@icloud.com>
2021-09-16 00:52:51 +0900

    delete: 不要なファイルを削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,15 +6,15 @@ module ApplicationHelper
   def user_rank(users)
     ranks.zip(users)
   end
-
+  
   def ruby_objects
     %w[obj1 obj2 obj3 obj4 obj5 obj6]
   end
-
+  
   def book_objects
     %w[book1 book2 book3 book4]
   end
-
+  
   def pen_objects
     %w[pen1 pen2 pen3 pen4 pen5 pen6]
   end
