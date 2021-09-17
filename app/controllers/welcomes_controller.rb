72074a901ced3742fe7ccd475bdce859019fc445
yohei-kanno <tkro.812.wkrni@icloud.com>
2021-09-17 14:48:55 +0900

    add: トップページの見た目を変更　

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -3,6 +3,6 @@ class WelcomesController < ApplicationController
 
   def new
     @categories = Category.all
-    @categories2 = Category.where(id: [1..4])
+    @categories2 = Category.where(id: [1,2,3,4,7])
   end
 end
