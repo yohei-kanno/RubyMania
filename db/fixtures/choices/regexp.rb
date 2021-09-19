31b357c767942f8955c8e8b66f8312028aee35b5
yohei-kanno <tkro.812.wkrni@icloud.com>
2021-09-19 11:55:26 +0900

    add: regexp_questions

--- db/fixtures/choices/regexp.rb
+++ db/fixtures/choices/regexp.rb
@@ -4,7 +4,823 @@ is_true = '「true」と表示される'
 is_false = '「false」と表示される'
 
 # r_question1
-$r_choice1 = '「#<MatchData "1">」と表示される'
-$r_choice2 = '「#<MatchData "12">」と表示される'
-$r_choice3 = '「#<MatchData "123">」と表示される'
-$r_choice4 = is_nil
+$r_choice = '「#<MatchData "1">」と表示される'
+$r_choice = '「#<MatchData "12">」と表示される'
+$r_choice = '「#<MatchData "123">」と表示される'
+$r_choice = is_nil
+
+# r_question2(3)
+$r_choice = '「String」と表示される'
+$r_choice = '「Integer」と表示される'
+$r_choice = '「Regexp」と表示される'
+$r_choice = is_exception
+
+# r_question3(1)
+$r_choice = is_true
+$r_choice = is_false
+$r_choice = is_nil
+$r_choice = is_exception
+
+# r_question4(2)
+$r_choice = is_true
+$r_choice = is_false
+$r_choice = is_nil
+$r_choice = is_exception
+
+# r_question5(3)
+$r_choice = '「#<MatchData "1">」と表示される'
+$r_choice = '「#<MatchData "12">」と表示される'
+$r_choice = '「#<MatchData "123">」と表示される'
+$r_choice = is_nil
+
+# r_question6(2)
+$r_choice = '「#<MatchData "1">」と表示される'
+$r_choice = '「#<MatchData "12">」と表示される'
+$r_choice = '「#<MatchData "123">」と表示される'
+$r_choice = is_nil
+
+# r_question7(3)
+$r_choice = '「#<MatchData "1">」と表示される'
+$r_choice = '「#<MatchData "12">」と表示される'
+$r_choice = '「#<MatchData "123">」と表示される'
+$r_choice = is_nil
+
+# r_question8(4)
+$r_choice = '「#<MatchData "1">」と表示される'
+$r_choice = '「#<MatchData "12">」と表示される'
+$r_choice = '「#<MatchData "123">」と表示される'
+$r_choice = is_nil
+
+# r_question9(4)
+$r_choice = '「#<MatchData "1">」と表示される'
+$r_choice = '「#<MatchData "12">」と表示される'
+$r_choice = '「#<MatchData "123">」と表示される'
+$r_choice = is_nil
+
+# r_question10
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question11
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question12
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question13
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question14
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question15
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question16
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question17
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question18
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question19
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question20
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question21
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question22
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question23
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question24
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+
+# r_question25
+$r_choice= ''
+$r_choice= ''
+$r_choice= ''
+$r_choice = ''
+
+# r_question26
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question27
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question28
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question29
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question30
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question31
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question32
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question33
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question34
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question35
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question36
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question37
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question38
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question39
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question40
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question41
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question42
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question43
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question44
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question45
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question46
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question47
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question48
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question49
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question50
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question51
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question52
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question53
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question54
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question55
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question56
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question57
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question58
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question59
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question60
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question61
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question62
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question63
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question64
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question65
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question66
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question67
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question68
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question69
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question70
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question71
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question72
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question73
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question74
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question75
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question76
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question77
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question78
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question79
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question80
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question81
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question82
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question83
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question84
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question85
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question86
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question87
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question88
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question89
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question90
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question91
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question92
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question93
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question94
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question95
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question96
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question97
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question98
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question99
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question100
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question101
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question102
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question103
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question104
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question105
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question106
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question107
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question108
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question109
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question110
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question111
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question112
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question113
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question114
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question115
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question116
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question117
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question118
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question119
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question120
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question121
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question122
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question123
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question124
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question125
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question126
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question127
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question128
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question129
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question130
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question131
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question132
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question133
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question134
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question135
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question136
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+
+# r_question137
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
+$r_choice = ''
