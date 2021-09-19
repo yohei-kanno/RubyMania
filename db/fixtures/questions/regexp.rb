51095ce65f93fa5f70e27dcf82c81eb1bff4633f
yohei-kanno <tkro.812.wkrni@icloud.com>
2021-09-19 11:56:19 +0900

    add: regexp_questions

--- db/fixtures/questions/regexp.rb
+++ db/fixtures/questions/regexp.rb
@@ -1,3 +1,320 @@
 $r_question1 =
   'obj = "123"
-  p obj.match(/\d/)'
+p obj.match(/\d/)'
+
+$r_question2 =
+  'obj = /123/
+p obj.class'
+
+$r_question3 =
+  'obj = /123/
+p obj.frozen?'
+
+$r_question4 =
+  'obj = Regexp.new("123")
+p obj.frozen?'
+
+$r_question5 =
+  'obj = "123"
+p obj.match(/.../)'
+
+$r_question6 =
+  'obj = "123"
+p obj.match(/\d{1,2}/)'
+
+$r_question7 =
+  'obj = "123"
+p obj.match(/\d{1,4}/)'
+
+$r_question8 =
+  'obj = "123"
+p obj.match(/\d{4,4}/)'
+
+$r_question9 =
+  'obj = "123"
+p obj.match(/\d{4}/)'
+
+$r_question10 =
+  ''
+
+$r_question11 =
+  ''
+
+$r_question12 =
+  ''
+
+$r_question13 =
+  ''
+
+$r_question14 =
+  ''
+
+$r_question15 =
+  ''
+
+$r_question16 =
+  ''
+
+$r_question17 =
+  ''
+
+$r_question18 =
+  ''
+
+$r_question19 =
+  ''
+
+$r_question20 =
+  ''
+
+$r_question21 =
+  ''
+
+$r_question22 =
+  ''
+
+$r_question23 =
+  ''
+
+$r_question24 =
+  ''
+
+$r_question25 =
+  ''
+
+$r_question26 =
+  ''
+
+$r_question27 =
+  ''
+
+$r_question28 =
+  ''
+
+$r_question29 =
+  ''
+
+$r_question30 =
+  ''
+
+$r_question31 =
+  ''
+
+$r_question32 =
+  ''
+
+$r_question33 =
+  ''
+
+$r_question34 =
+  ''
+
+$r_question35 =
+  ''
+
+$r_question36 =
+  ''
+
+$r_question37 =
+  ''
+
+$r_question38 =
+  ''
+
+$r_question39 =
+  ''
+
+$r_question40 =
+  ''
+
+$r_question41 =
+  ''
+
+$r_question42 =
+  ''
+
+$r_question43 =
+  ''
+
+$r_question44 =
+  ''
+
+$r_question45 =
+  ''
+
+$r_question46 =
+  ''
+
+$r_question47 =
+  ''
+
+$r_question48 =
+  ''
+
+$r_question49 =
+  ''
+
+$r_question50 =
+  ''
+
+$r_question51 =
+  ''
+
+$r_question52 =
+  ''
+
+$r_question53 =
+  ''
+
+$r_question54 =
+  ''
+
+$r_question55 =
+  ''
+
+$r_question56 =
+  ''
+
+$r_question57 =
+  ''
+
+$r_question58 =
+  ''
+
+$r_question59 =
+  ''
+
+$r_question60 =
+  ''
+
+$r_question61 =
+  ''
+
+$r_question62 =
+  ''
+
+$r_question63 =
+  ''
+
+$r_question64 =
+  ''
+
+$r_question65 =
+  ''
+
+$r_question66 =
+  ''
+
+$r_question67 =
+  ''
+
+$r_question68 =
+  ''
+
+$r_question69 =
+  ''
+
+$r_question70 =
+  ''
+
+$r_question71 =
+  ''
+
+$r_question72 =
+  ''
+
+$r_question73 =
+  ''
+
+$r_question74 =
+  ''
+
+$r_question75 =
+  ''
+
+$r_question76 =
+  ''
+
+$r_question77 =
+  ''
+
+$r_question78 =
+  ''
+
+$r_question79 =
+  ''
+
+$r_question80 =
+  ''
+
+$r_question81 =
+  ''
+
+$r_question82 =
+  ''
+
+$r_question83 =
+  ''
+
+$r_question84 =
+  ''
+
+$r_question85 =
+  ''
+
+$r_question86 =
+  ''
+
+$r_question87 =
+  ''
+
+$r_question88 =
+  ''
+
+$r_question89 =
+  ''
+
+$r_question90 =
+  ''
+
+$r_question91 =
+  ''
+
+$r_question92 =
+  ''
+
+$r_question93 =
+  ''
+
+$r_question94 =
+  ''
+
+$r_question95 =
+  ''
+
+$r_question96 =
+  ''
+
+$r_question97 =
+  ''
+
+$r_question98 =
+  ''
+
+$r_question99 =
+  ''
+
+$r_question100 =
+  ''
+
+$r_question101 =
+  ''
+
+$r_question102 =
+  ''
+
+$r_question103 =
+  ''
+
+$r_question104 =
+  ''
