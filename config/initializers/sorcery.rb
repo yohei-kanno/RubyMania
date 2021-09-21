205313cd2621b74a548a8e9458f0a332ccaa13e2
yohei-kanno <tkro.812.wkrni@icloud.com>
2021-09-21 22:15:35 +0900

    Fix: sorceryの設定を変更

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -7,13 +7,14 @@ Rails.application.config.sorcery.configure do |config|
   end
 
   config.user_class = 'User'
+  config.external_providers = [:github]
+  
   if  Rails.env.production?
-    config.github.key = Rails.application.credentials.dig(:github_productioin, :key)
-    config.github.secret = Rails.application.credentials.dig(:github_productioin, :secret)
-    config.github.callback_url = Rails.application.credentials.dig(:github_productioin, :callback_url)
+    config.github.key = Rails.application.credentials.dig(:github_production, :key)
+    config.github.secret = Rails.application.credentials.dig(:github_production, :secret)
+    config.github.callback_url = Rails.application.credentials.dig(:github_production, :callback_url)
   end
   
-  config.external_providers = [:github]
   config.github.user_info_mapping = {email: "email", name: "login", remote_avatar_url: "avatar_url"}
   config.github.scope = "user:email"
 end
