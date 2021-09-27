f660f4cd9fcd8d8ff2eeae79436fbf94f99163c2
yohei-kanno <tkro.812.wkrni@icloud.com>
2021-09-27 16:11:33 +0900

    add: トップページでスクロール機能追加

--- app/javascript/packs/swiper/swiper.js
+++ app/javascript/packs/swiper/swiper.js
@@ -6,37 +6,10 @@ const swiper = new Swiper('.swiper', {
     delay: 3000,
   },
   direction: "horizontal",
-  effect: "coverflow",
+  effect: 'coverflow',
   pagination: {
     el: '.swiper-pagination',
     type: "bullets",
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
-const swiper2 = new Swiper('.swiper2', {
-  direction: 'vertical',
-  loop: true,
-  speed: 500,
-  autoplay: {
-    delay: 3000,
-  },
-  direction: "horizontal",
-  effect: "flip",
-  pagination: {
-    el: '.swiper-pagination',
-    type: "fraction",
-    renderBullet: function (index, className) {
-			return '<span class="' + className + '">' + (index + 1) + '</span>';
-		}
   },
 
   navigation: {
