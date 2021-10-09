9cb1b4de9df6188e6ed53c90dbc148199aa22936
yohei-kanno <tkro.812.wkrni@icloud.com>
2021-10-09 16:00:01 +0900

    add: id順に並び替え

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -3,7 +3,7 @@ class WelcomesController < ApplicationController
 
   def new
     @r1 = 100
-    @categories = Category.all
+    @categories = Category.all.order(:id)
     @categories2 = Category.where.not(name: "基礎編総合問題")
   end
 end
