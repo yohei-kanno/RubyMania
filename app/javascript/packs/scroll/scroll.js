ae66c6709748d98c62fd557be08791f2256765ee
yohei-kanno <tkro.812.wkrni@icloud.com>
2021-10-10 15:06:38 +0900

    add: スクロールエフェクト機能追加

---
+++ app/javascript/packs/scroll/scroll.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal(`.question-effect`,{
+  duration: 1500,
+  reset: false,
+  origin: 'top',
+  distance: '50px'
+});
+
+ScrollReveal().reveal(`.top-effect`,{
+  duration: 1500,
+});
