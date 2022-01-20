b1846e2b074eac39b86b08c6caff8d44e1627412
youheikanno <tkro.812.wkrni@icloud.com>
2022-01-20 15:18:57 +0900

    add: 問題と解答フォームにエフェクト追加

--- app/javascript/packs/scroll/scroll.js
+++ app/javascript/packs/scroll/scroll.js
@@ -8,3 +8,19 @@ ScrollReveal().reveal(`.question-effect`,{
 ScrollReveal().reveal(`.top-effect`,{
   duration: 1500,
 });
+
+ScrollReveal().reveal(`.question-form-effect`,{
+  duration: 500,
+  reset: false,
+  origin: 'right',
+  distance: '100%',
+  viewFactor: 0
+});
+
+ScrollReveal().reveal(`.answer-form-effect`,{
+  duration: 1500,
+  reset: false,
+  origin: 'bottom',
+  distance: '100px',
+  viewFactor: 0
+});
