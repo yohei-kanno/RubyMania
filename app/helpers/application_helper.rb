07c6151047e66fa98e8997f2d6d06af7fade85d6
youheikanno <tkro.812.wkrni@icloud.com>
2022-01-29 11:06:14 +0900

    Fix: ogp画像の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
       description: 'Rubyに特化したクイズサービスです。問題数は３５０問以上！progateでruby終わった！もしくは少し肩慣らししたいという方に向けてのサービスです。',
       canonical: "https://www.rubymania.info",
       icon: [
-        { href: image_url('top.png') },
+        { href: image_url('logo.png') },
         { href: image_url('logo.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' }
       ],
       og: {
@@ -24,6 +24,10 @@ module ApplicationHelper
         url: "https://www.rubymania.info",
         image: image_url('top.png'),
         locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary',
+        site: '@iZXIlbQrHHr5n6E',
       }
     }
   end
