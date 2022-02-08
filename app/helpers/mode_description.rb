5004ee86d6b9575b9dc81f93743c4b8ebf7aaf4c
youheikanno <tkro.812.wkrni@icloud.com>
2022-02-08 22:33:24 +0900

    add: 表記を追加

--- app/helpers/mode_description.rb
+++ app/helpers/mode_description.rb
@@ -50,6 +50,7 @@ module ModeDescription
       content_tag(:div) do
         concat content_tag(:div, "ランダムに10問出題されます", class: "category-title")
         concat content_tag(:div, "・記録、チャート、偏差値に反映されます(ログインが必要です)", class: "term-sentence")
+        concat content_tag(:div, "・カレンダーにも記録されます(ログインが必要です)", class: "term-sentence")
         concat content_tag(:div, "・ある程度がっつりやりたい方にお勧めでです。", class: "term-sentence")
         concat content_tag(:div, "下の画像からカテゴリーを選択して下さい。", class: "term-sentence mt-3")
       end
@@ -58,6 +59,7 @@ module ModeDescription
       content_tag(:div) do
         concat content_tag(:div, "ランダムに3問出題されます", class: "category-title")
         concat content_tag(:div, "・記録やチャートには反映されませんが経験値は付与されます。", class: "term-sentence")
+        concat content_tag(:div, "・カレンダーには反映されません", class: "term-sentence")
         concat content_tag(:div, "・経験値を得る為にはログインが必要です。", class: "term-sentence")
         concat content_tag(:div, "・1問では物足りないけど10問は重い方にお勧めです。", class: "term-sentence")
         concat content_tag(:div, "下の画像からカテゴリーを選択して下さい。", class: "term-sentence mt-3")
@@ -67,6 +69,7 @@ module ModeDescription
       content_tag(:div) do
         concat content_tag(:div, "ランダムに1問1答形式で出題されます", class: "category-title")
         concat content_tag(:div, "・記録やチャートには反映されませんが経験値は付与されます。", class: "term-sentence")
+        concat content_tag(:div, "・カレンダーには反映されません", class: "term-sentence")
         concat content_tag(:div, "・経験値を得る為にはログインが必要です。", class: "term-sentence")
         concat content_tag(:div, "・サクッと問題を解いていきたい方にお勧めです。", class: "term-sentence")
         concat content_tag(:div, "下の画像からカテゴリーを選択して下さい。", class: "term-sentence mt-3")
